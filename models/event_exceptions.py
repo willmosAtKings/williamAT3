4f2f9906cf3fcb54751f4e2fd3d4276522205a6a
William <moses_wi@student.kings.edu.au>
2025-07-15 04:27:38 +1000

    Added 'event_exceptions.py' to account for how events are edited - recurring, and multiday events are detected separately to single day events and handled differently

---
+++ models/event_exceptions.py
@@ -0,0 +1,16 @@
+from extensions import db
+
+class EventException(db.Model):
+    __tablename__ = 'event_exceptions'
+
+    id = db.Column(db.Integer, primary_key=True)
+    original_event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
+    exception_date = db.Column(db.Date, nullable=False)  # The date this exception applies to
+    
+    # If these are NULL, it means the occurrence is deleted
+    title = db.Column(db.String(150), nullable=True)
+    description = db.Column(db.String(300), nullable=True)
+    priority = db.Column(db.Integer, nullable=True)
+    tags = db.Column(db.String(100), nullable=True)
+    start_time = db.Column(db.DateTime, nullable=True)
+    end_time = db.Column(db.DateTime, nullable=True)
