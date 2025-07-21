f7e41ea8c5b72ec88e4eb4c0466943d7f1e2cdc3
William <moses_wi@student.kings.edu.au>
2025-07-21 19:51:59 +1000

    Final commit

--- scheduler.py
+++ scheduler.py
@@ -6,13 +6,12 @@ def start_scheduler(app):
 
     # Schedule the job every 30 seconds (for testing; normally use cron at 7am)
 
-# trigger='cron',
-# hour=7,
-# minute=0
+
     scheduler.add_job(
         func=lambda: send_event_reminders(app),
-        trigger='interval',
-        seconds=30,
+        trigger='cron',
+        hour=7,
+        minute=0,
         id='daily_notification_job',
         replace_existing=True
     )
